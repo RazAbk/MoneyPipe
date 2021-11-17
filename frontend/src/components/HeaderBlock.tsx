a185c7e16ecc525cca6f3fcdb3bf4a29a1a8425d
RazAbk <RazAb98754@Gmail.com>
2021-11-17 19:02:12 +0200

    header block is finished, responsive & functional

--- frontend/src/components/HeaderBlock.tsx
+++ frontend/src/components/HeaderBlock.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store/store'
+import { FaFilter } from 'react-icons/fa'
 
 export const HeaderBlock = () => {
+
+    const currentViewMode = useSelector((state: RootState) => state.userModule.currentViewMode)
+    const currentLabel = useSelector((state: RootState) => state.userModule.currentLabel)
+
     return (
         <div className="header-block">
-            .
+            <div className="header-block-content">
+                {currentLabel && <h2>{currentLabel}</h2>}
+                {(!currentLabel && currentViewMode) && <h2>{currentViewMode}</h2>}
+                <p>{currentViewMode} of <u>{'*Time*'}</u> incomes and expenses</p>
+            </div>
+            <div className="filter-btn">
+                <FaFilter/>
+            </div>
         </div>
     )
 }
