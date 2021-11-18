e4e245aa1c1953663cba1ec9f5b9b430c611b642
RazAbk <RazAb98754@Gmail.com>
2021-11-18 14:57:41 +0200

    app main page + menus and fully responsive

--- frontend/src/pages/MainApp.tsx
+++ frontend/src/pages/MainApp.tsx
@@ -8,18 +8,28 @@ import { getData } from '../store/actions/user.action'
 import { BalanceBlock } from '../components/BalanceBlock'
 import { IncomesBlock } from '../components/IncomesBlock'
 import { ExpensesBlock } from '../components/ExpensesBlock'
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import { GoPrimitiveDot } from 'react-icons/go'
 
 
 export const MainApp = () => {
 
     const [isMenuOpen, setMenuOpen] = useState(false)
+    const [currentBlock, setCurrentBlock] = useState(0)
+    const [ref] = useKeenSlider<HTMLDivElement>({
+        spacing: 16,
+        initial: 0,
+        slideChanged(s) {
+            setCurrentBlock(s.details().relativeSlide)
+        }
+    })
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getData())
 
     }, [dispatch])
-
 
     return (
         <>
@@ -32,9 +42,18 @@ export const MainApp = () => {
 
                         <HeaderBlock />
                         <BalanceBlock />
-                        <div className="main-content-blocks">
-                            <IncomesBlock/>
-                            <ExpensesBlock/>
+                        <div ref={ref} className="main-content-blocks mobile-only keen-slider">
+                            <div className="pagination-dots">
+                                <GoPrimitiveDot className={`${currentBlock === 1 ? 'fade-dot' : ''}`}/>
+                                <GoPrimitiveDot className={`${currentBlock === 0 ? 'fade-dot' : ''}`}/>
+                            </div>
+                            <IncomesBlock />
+                            <ExpensesBlock />
+                        </div>
+
+                        <div className="main-content-blocks above-mobile">
+                            <IncomesBlock />
+                            <ExpensesBlock />
                         </div>
                     </div>
 
