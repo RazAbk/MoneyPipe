c290509483e7ca223cdacbf8c80160769560b3d1
RazAbk <RazAb98754@Gmail.com>
2021-11-24 22:55:31 +0200

    remove unused imports

--- frontend/src/components/HeaderBlock.tsx
+++ frontend/src/components/HeaderBlock.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
 import { BiSearchAlt } from 'react-icons/bi'
