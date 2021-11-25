d7d43df096f25d7dde8800e5e3d39c1e92204d0e
RazAbk <RazAb98754@Gmail.com>
2021-11-25 11:30:12 +0200

    clean react comments

--- frontend/src/components/GraphBlock.tsx
+++ frontend/src/components/GraphBlock.tsx
@@ -1,46 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { IDataMap, IDataObject, IPieData } from '../interfaces/dataInterfaces'
-import { RootState } from '../store/store'
-import { Line } from 'react-chartjs-2'
+// import React, { useEffect, useState } from 'react'
+// import { useSelector } from 'react-redux'
+// import { IDataMap, IDataObject, IPieData } from '../interfaces/dataInterfaces'
+// import { RootState } from '../store/store'
+// import { Line } from 'react-chartjs-2'
 
-const options: any = {
-    legend: {
-        display: true,
-        position: "right"
-    },
-    scales: {
-        yAxes: [
-            {
-                scaleLabel: {
-                    display: true
-                },
-                ticks: {
-                    display: true,
-                    minRotation: 0
-                }
-            }
-        ],
-        xAxes: [
-            {
-                gridLines: {
-                    display: false
-                },
-                scaleLabel: {
-                    display: true
-                },
-                ticks: {
-                    display: true,
-                    minRotation: 90
-                }
-            }
-        ]
-    }
-};
+// const options: any = {
+//     legend: {
+//         display: true,
+//         position: "right"
+//     },
+//     scales: {
+//         yAxes: [
+//             {
+//                 scaleLabel: {
+//                     display: true
+//                 },
+//                 ticks: {
+//                     display: true,
+//                     minRotation: 0
+//                 }
+//             }
+//         ],
+//         xAxes: [
+//             {
+//                 gridLines: {
+//                     display: false
+//                 },
+//                 scaleLabel: {
+//                     display: true
+//                 },
+//                 ticks: {
+//                     display: true,
+//                     minRotation: 90
+//                 }
+//             }
+//         ]
+//     }
+// };
 
 export const GraphBlock = () => {
 
-    const rawData: IDataObject = useSelector((state: RootState) => state.userModule.data)
+    // const rawData: IDataObject = useSelector((state: RootState) => state.userModule.data)
 
     // const data = {
     //     labels: 
