676008b145ec14624bd37dc6e817a50950326b99
RazAbk <RazAb98754@Gmail.com>
2021-11-29 15:35:55 +0200

    add actions edit & delete buttons

--- frontend/src/components/ActionPreview.tsx
+++ frontend/src/components/ActionPreview.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { IAction, IDataObject } from '../interfaces/dataInterfaces'
 import { utilService } from '../services/util.service'
 import { RootState } from '../store/store'
 import { GetIcon } from './GetIcon'
+import { FaRegEdit } from 'react-icons/fa'
+import { VscTrash } from 'react-icons/vsc'
+
 
 interface IActionProps {
     action: IAction
@@ -14,6 +17,26 @@ export const ActionPreview = ({ action }: IActionProps) => {
 
     const rawData: IDataObject = useSelector((state: RootState) => state.userModule.data)
 
+    const [showActions, setShowActions] = useState(false)
+
+    const ref = useRef(null)
+
+    useEffect(() => {
+        const closeActions = () => {
+            setShowActions(false)
+        }
+
+        if(ref){
+            if(ref.current){
+                const el: HTMLDivElement = ref.current
+                el.addEventListener('blur', closeActions)
+
+                return () => {
+                    el.removeEventListener('blur', closeActions)
+                }
+            }
+        }
+    }, [])
 
     const findCategoryData = (category: string) => {
         if(rawData){
@@ -26,7 +49,7 @@ export const ActionPreview = ({ action }: IActionProps) => {
 
     if(!categoryData) return <h1>Loading</h1>
     return (
-        <div className="action-preview">
+        <div ref={ref} tabIndex={-1} className="action-preview" onClick={() => {setShowActions(prevState => !prevState)}}>
             <div className="left-side">
             <div className="action-details-icon" style={{ backgroundColor: categoryData.bgColor }}>
                 <GetIcon iconName={categoryData.icon}/>
@@ -39,6 +62,10 @@ export const ActionPreview = ({ action }: IActionProps) => {
             </div>
             <div className="right-side">
                 <h3>{action.amount.toLocaleString()}{rawData.currencySign}</h3>
+            </div>
+            <div className="action-preview-actions" style={{transform: showActions ? 'translateX(0%)' : 'translateX(100%)'}}>
+                <button><FaRegEdit/></button>
+                <button><VscTrash/></button>
             </div>
         </div>
     )
