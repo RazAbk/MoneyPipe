41361a4ec69008236ba77b9923d2c4ee816c625d
RazAbk <RazAb98754@Gmail.com>
2021-12-05 15:04:19 +0200

    create basic express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,12 @@
-console.log('hello from the server')
+import express from 'express';
+
+const app = express();
+const PORT = 3030;
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
