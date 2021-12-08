8016152b7eb92ec173ba1c51ae8c11360720c23b
RazAbk <RazAb98754@Gmail.com>
2021-12-08 15:43:47 +0200

    add uploadimage function

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -14,7 +14,8 @@ export const userService = {
     addAction,
     deleteAction,
     addCategory,
-    addLabel
+    addLabel,
+    uploadImg
 }
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? '' : '//localhost:3030'
@@ -48,10 +49,10 @@ async function logout() {
 // CURD Functions
 
 async function getData(filterBy: IDateFilterBy) {
-    try{
+    try {
         const res = await axios.get(`${BASE_URL}/api/user/data?startDate=${filterBy.startDate}&endDate=${filterBy.endDate}`)
         return res.data
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 }
@@ -67,11 +68,11 @@ async function addAction(action: IAction) {
 }
 
 async function deleteAction(actionId: string) {
-    try{
+    try {
         const res = await axios.delete(`${BASE_URL}/api/user/action/${actionId}`)
         sessionStorageService.save('loggedInUser', res.data)
         return res.data
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 }
@@ -91,6 +92,29 @@ async function addLabel(label: ILabel) {
         const res = await axios.post(`${BASE_URL}/api/user/label`, label)
         sessionStorageService.save('loggedInUser', res.data)
         return res.data
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function uploadImg(file: File) {
+    try {
+        const CLOUD_NAME = 'dfj4zd14o'
+        const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+
+        const formData = new FormData();
+        formData.append('file', file)
+        formData.append('upload_preset', 'k1qiifqe');
+
+        return fetch(UPLOAD_URL, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(res => {
+                return res.url
+            })
+            .catch(err => console.error(err))
     } catch (err) {
         console.log(err)
     }
