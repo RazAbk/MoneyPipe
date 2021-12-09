bedd354df2b349fdef0d637852d5a4218fa6fb55
RazAbk <RazAb98754@Gmail.com>
2021-12-10 00:11:38 +0200

    remove problematic charts

--- backend/src/services/util.service.ts
+++ backend/src/services/util.service.ts
@@ -4,7 +4,7 @@ module.exports = {
 
 function makeId(length: number = 6) {
     let txt = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
 
     for (let i = 0; i < length; i++) {
         txt += possible.charAt(Math.floor(Math.random() * possible.length));
