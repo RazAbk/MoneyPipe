df1af1a16488bd4f42ba1cf7932eb5e495ddd305
RazAbk <RazAb98754@Gmail.com>
2021-12-10 13:45:16 +0200

    add function that accepts title

--- frontend/src/services/alert.service.ts
+++ frontend/src/services/alert.service.ts
@@ -1,13 +1,13 @@
-import { width } from '@mui/system';
 import { store } from 'react-notifications-component';
 
 export interface IAlertMessage{
     message: string;
     type: "success" | "danger" | "info" | "default" | "warning" | undefined;
     duration: number;
+    title: string;
 }
 
-export function alertMessage(message: string, type: IAlertMessage["type"], duration: number = 0) {
+export function alertMessage(message: string, type: IAlertMessage["type"], duration: number = 3500) {
     return store.addNotification({
         message,
         type,
@@ -21,6 +21,24 @@ export function alertMessage(message: string, type: IAlertMessage["type"], durat
         }
     });
 }
+
+export function alertTitleMessage(title: string, message: string, type: IAlertMessage["type"], duration: number = 3500) {
+    return store.addNotification({
+        title,
+        message,
+        type,
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__backInRight"],
+        animationOut: ["animate__animated", "animate__backOutRight"],
+        dismiss: {
+            duration,
+            onScreen: true
+        }
+    });
+}
+
+
 
 export function removeMessage(messageId: string) {
     store.removeNotification(messageId)
