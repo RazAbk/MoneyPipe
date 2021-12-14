95272dfdc65575dbf4ff5c296b72b49a2f3b9151
RazAbk <RazAb98754@Gmail.com>
2021-12-14 22:38:22 +0200

    add caching to serviceWorker

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,18 +1,47 @@
-const self = this
+const self = this;
 
-console.log('got to serviceWorker')
+const staticCacheName = "static-assets-v1";
+// const dynamicCache = 'dynamic-assets-v1'
 
-// Install serviceWorkerasd
-self.addEventListener('install', event => {
-    console.log('serviceWorker has been installed')
-})
+const assets = ["/", "/mydata"];
+
+// Install serviceWorker
+self.addEventListener("install", (event) => {
+  // Waits until serviceWorker finishes to cache the requests(assets),
+  // Only then finishes the install process (async...)
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
 
 // Listen for the activate event
-self.addEventListener('activate', event => {
-    console.log('serviceWorker has been activated')
-})
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    // Delete the old cache versions, except the latest cache
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete())
+      );
+    })
+  );
+});
 
 // Fetch event
-self.addEventListener('fetch', event => {
-    // console.log('fetch event occurred', event)
-})
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request)
+        // Caching for static values (isnt suitable for MoneyPipe...)
+//       .then(response => {
+//           return caches.open(dynamicCache).then(cache => {
+//               cache.put(event.request.url, response.clone())
+//               return response
+//           })
+//       })
+    })
+  );
+});
