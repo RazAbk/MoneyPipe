6f3d2fc62f4c04c7c02e39a8c1366cdcba95331e
RazAbk <RazAb98754@Gmail.com>
2021-12-25 20:54:26 +0200

    commit compressed build to production

--- backend/src/public/loadServiceWorker.js
+++ backend/src/public/loadServiceWorker.js
@@ -1,6 +1,4 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then((reg) => console.log("serviceWorker registered!", reg))
-      .catch((err) => console.log("serviceWorker not registered!", err));
   }
